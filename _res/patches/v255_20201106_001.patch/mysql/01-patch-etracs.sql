7d225cbdb4af7f53d5b0403fc36a8fd96c2b14e7
worgflores <worgflores@gmail.com>
2020-11-12 17:06:59 +0800

    appserver: epayment: updated

--- _res/patches/v255_20201106_001.patch/mysql/01-patch-etracs.sql
+++ _res/patches/v255_20201106_001.patch/mysql/01-patch-etracs.sql
@@ -15,3 +15,6 @@ create index ix_refid on sys_email_queue (refid);
 create index ix_state on sys_email_queue (state); 
 create index ix_reportid on sys_email_queue (reportid); 
 create index ix_dtsent on sys_email_queue (dtsent); 
+
+
+alter table sys_email_queue add connection varchar(50) null ;
