d2c7913aaeea3100b26f6c4dbb2b8c7ba6bd4d56
Jessie Zamora <jzamss@gmail.com>
2021-03-29 19:56:21 +0800

    RPTIS: v255-03026

---
+++ _res/patches/v255_20210215.patch/mysql/01-patch-etracs.sql.sql
@@ -0,0 +1,118 @@
+
+CREATE TABLE `rpt_syncdata_forsync` (
+  `objid` varchar(50) NOT NULL,
+  `reftype` varchar(50) NOT NULL,
+  `refno` varchar(50) NOT NULL,
+  `action` varchar(50) NOT NULL,
+  `orgid` varchar(50) NOT NULL,
+  `dtfiled` datetime NOT NULL,
+  `createdby_objid` varchar(50) DEFAULT NULL,
+  `createdby_name` varchar(255) DEFAULT NULL,
+  `createdby_title` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`objid`),
+  KEY `ix_refno` (`refno`),
+  KEY `ix_orgid` (`orgid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+CREATE TABLE `rpt_syncdata` (
+  `objid` varchar(50) NOT NULL,
+  `state` varchar(25) NOT NULL,
+  `refid` varchar(50) NOT NULL,
+  `reftype` varchar(50) NOT NULL,
+  `refno` varchar(50) NOT NULL,
+  `action` varchar(50) NOT NULL,
+  `dtfiled` datetime NOT NULL,
+  `orgid` varchar(50) NOT NULL,
+  `remote_orgid` varchar(50) DEFAULT NULL,
+  `remote_orgcode` varchar(5) DEFAULT NULL,
+  `remote_orgclass` varchar(25) DEFAULT NULL,
+  `sender_objid` varchar(50) DEFAULT NULL,
+  `sender_name` varchar(255) DEFAULT NULL,
+  `sender_title` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`objid`),
+  KEY `ix_state` (`state`),
+  KEY `ix_refid` (`refid`),
+  KEY `ix_refno` (`refno`),
+  KEY `ix_orgid` (`orgid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+CREATE TABLE `rpt_syncdata_item` (
+  `objid` varchar(50) NOT NULL,
+  `parentid` varchar(50) NOT NULL,
+  `state` varchar(25) NOT NULL,
+  `refid` varchar(50) NOT NULL,
+  `reftype` varchar(50) NOT NULL,
+  `refno` varchar(50) NOT NULL,
+  `action` varchar(50) NOT NULL,
+  `idx` int(11) NOT NULL,
+  `info` text,
+  PRIMARY KEY (`objid`),
+  KEY `ix_parentid` (`parentid`),
+  KEY `ix_state` (`state`),
+  KEY `ix_refid` (`refid`),
+  KEY `ix_refno` (`refno`),
+  CONSTRAINT `FK_parentid_rpt_syncdata` FOREIGN KEY (`parentid`) REFERENCES `rpt_syncdata` (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+CREATE TABLE `rpt_syncdata_error` (
+  `objid` varchar(50) NOT NULL,
+  `filekey` varchar(1000) NOT NULL,
+  `error` text,
+  `refid` varchar(50) NOT NULL,
+  `reftype` varchar(50) NOT NULL,
+  `refno` varchar(50) NOT NULL,
+  `action` varchar(50) NOT NULL,
+  `idx` int(11) NOT NULL,
+  `info` text,
+  `parent` text,
+  `remote_orgid` varchar(50) DEFAULT NULL,
+  `remote_orgcode` varchar(5) DEFAULT NULL,
+  `remote_orgclass` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`objid`),
+  KEY `ix_refid` (`refid`),
+  KEY `ix_refno` (`refno`),
+  KEY `ix_filekey` (`filekey`(255)),
+  KEY `ix_remote_orgid` (`remote_orgid`),
+  KEY `ix_remote_orgcode` (`remote_orgcode`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+INSERT INTO `sys_var` (`name`, `value`, `description`, `datatype`, `category`) 
+VALUES ('assesser_new_sync_lgus', NULL, 'List of LGUs using new sync facility', NULL, 'ASSESSOR')
+;
+
+
+
+ALTER TABLE rpt_syncdata_forsync ADD remote_orgid VARCHAR(15)
+;
+
+
+INSERT INTO `sys_var` (`name`, `value`, `description`, `datatype`, `category`) VALUES ('fileserver_upload_task_active', '0', 'Activate / Deactivate upload task', 'boolean', 'SYSTEM')
+;
+
+
+INSERT INTO `sys_var` (`name`, `value`, `description`, `datatype`, `category`) 
+VALUES ('fileserver_download_task_active', '1', 'Activate / Deactivate download task', 'boolean', 'SYSTEM')
+;
+
+
+CREATE TABLE `rpt_syncdata_completed` (
+  `objid` varchar(255) NOT NULL,
+  `idx` int(255) DEFAULT NULL,
+  `action` varchar(100) DEFAULT NULL,
+  `refno` varchar(50) DEFAULT NULL,
+  `refid` varchar(50) DEFAULT NULL,
+  `reftype` varchar(50) DEFAULT NULL,
+  `parent_orgid` varchar(50) DEFAULT NULL,
+  `sender_name` varchar(255) DEFAULT NULL,
+  `sender_title` varchar(255) DEFAULT NULL,
+  `dtcreated` datetime DEFAULT NULL,
+  PRIMARY KEY (`objid`),
+  KEY `ix_refno` (`refno`),
+  KEY `ix_refid` (`refid`),
+  KEY `ix_parent_orgid` (`parent_orgid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
